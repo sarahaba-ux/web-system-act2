47b4b14e6273d6ec648debf09bc4d75dc76d4d52
sarahaba-ux <158169590+sarahaba-ux@users.noreply.github.com>
2024-01-31 01:00:24 +0800

    Add files via upload

---
+++ ataiza_abane_act2/script.js
@@ -0,0 +1,10 @@
+function handleButtonClick(box) {
+    alert(`Sport: Karatedo\nHobbies: Watching kdramas and Drawing`);
+    // You can perform additional actions here based on the box (left or right)
+}
+
+function handleButtonClick1(box) {
+    alert(`Sport: Swimming\nHobbies: Playing games and Watching movies`);
+    // You can perform additional actions here based on the box (left or right)
+}
+
